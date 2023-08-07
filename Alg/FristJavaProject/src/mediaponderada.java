1c5b077e1bab22ca3aab57eece7ca94a7ed9ad9b
Thiagoprepaz <profissionalcontactothiago@gmail.com>
2023-08-06 22:55:16 -0400

    inicio do projeto para conversão de tempo

--- Alg/FristJavaProject/src/mediaponderada.java
+++ Alg/FristJavaProject/src/mediaponderada.java
@@ -1,4 +1,4 @@
-public class App {
+public class mediaponderada {
 
     public static void main(String[] args) throws Exception {
         float n1, n2, n3, n4, media;
---
+++ Alg/FristJavaProject/src/tempo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class tempo {
+    Scanner input = new Scanner(System.in);
+
+    int time; // le os segundos
+    System.out.println("Enter industrial process time: "); // read the iput value
+    time = input.nextInt(); // reads the time
+
+
+}
