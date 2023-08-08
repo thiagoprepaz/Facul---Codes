373dae278e43bb6522315c93bb594580da9babc5
Thiagoprepaz <profissionalcontactothiago@gmail.com>
2023-08-08 07:53:28 -0400

    Código rodando porém com erro na leitura

--- Alg/FristJavaProject/src/tempo.java
+++ Alg/FristJavaProject/src/tempo.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
-public class tempo {
-    Scanner input = new Scanner(System.in);
+    
+    public class tempo {
+          public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
 
-    int time; // le os segundos
-    System.out.println("Enter industrial process time: "); // read the iput value
-    time = input.nextInt(); // reads the time
+             int time; // le os segundos
+            System.out.println("Enter industrial process time: "); // read the iput value
+            time = input.nextInt(); // reads the time
+            System.out.println(time);
 
-
+    }   
 }
