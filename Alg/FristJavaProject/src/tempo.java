1b7e3dd242329f63bfb0582741f306d9974530ef
Thiagoprepaz <profissionalcontactothiago@gmail.com>
2023-08-20 11:21:19 -0400

    tempo funcional

--- Alg/FristJavaProject/src/tempo.java
+++ Alg/FristJavaProject/src/tempo.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
     
     public class tempo {
-          public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
+      public static void main(String[] args)  throws Exception {
+            Scanner input= new Scanner(System.in);
 
              int time; // le os segundos
             System.out.println("Enter industrial process time: "); // read the iput value
             time = input.nextInt(); // reads the time
-            System.out.println(time);
+            System.out.print("Tempo em segundos: " + time);
 
     }   
 }
