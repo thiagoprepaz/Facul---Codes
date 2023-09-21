33698d5d3537735bf904e6be754bed1100971154
Thiagoprepaz <profissionalcontactothiago@gmail.com>
2023-09-21 14:17:06 -0400

    app cigarro

---
+++ Estudos/src/Lista3/cigarrosfumados.java
@@ -0,0 +1,30 @@
+package Lista3;
+
+import java.util.Scanner;
+import java.lang.Math; // I think it´s gona be used in the final print 
+
+public class cigarrosfumados {
+    /*
+     * Faça um programa em Java que calcula a quantidade dinheiro gasta por um
+     * fumante. O programa deve ler o nome do fumante, o número de anos que ele
+     * fumou, o nº de cigarros fumados por dia e o preço de uma carteira com 20
+     * cigarros. Considere que todos os meses possuem 30 dias.
+     * 
+     * · Exemplo de entrada:
+     * 
+     * o Qual o nome do fumante? João
+     * 
+     * o Quantos anos João fumou? 25
+     * 
+     * o Qual a quantidade média de cigarros por dia? 12
+     * 
+     * o Qual o preço médio de cada carteira? 2,67
+     * 
+     * · Saída esperada:
+     * 
+     * o João já gastou um total de 14.418 reais.
+     */
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
