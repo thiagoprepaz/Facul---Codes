f60d443f2bf1d77c6f128d5467f483994f2a9322
Thiagoprepaz <profissionalcontactothiago@gmail.com>
2023-10-05 19:45:35 -0400

    codigo funcional

--- Estudos/src/Lista3/cigarrosfumados.java
+++ Estudos/src/Lista3/cigarrosfumados.java
@@ -25,6 +25,26 @@ public class cigarrosfumados {
      * o João já gastou um total de 14.418 reais.
      */
     public static void main(String[] args) {
-        System.out.println();
+        Scanner input = new Scanner(System.in);//scanner object
+
+        String name;
+        int years, numparcial;
+        double price, total;
+
+        System.out.print("What's your name? ");
+        name = input.nextLine();//read the smoker name
+
+        System.out.print("How many years did you smoke? ");
+        years = input.nextInt();//how many years he smoked(all the months are of 30 days)
+
+        System.out.print("How many cigarettes did you smoke per day? ");
+        numparcial = input.nextInt();//number of cigarettes per day
+
+        System.out.print("What's the price of a cigarette pack? ");
+        price = input.nextDouble();//price of a cigarette pack with 20 cigarettes
+
+        total = ((((((years*12)*30)*numparcial))/20)*price);//calculo de dinheiro gasto
+
+        System.out.println("O/A " + name + " gastou um total de " + total + "reais");
     }
 }
